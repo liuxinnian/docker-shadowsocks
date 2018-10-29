d223a15e7ceaa40f76eb9544ba7d8db289ae26e5
Xinnian Liu <uplinux@gmail.com>
2018-10-29 12:16:55 +0000

    up to date

--- install-shadowsocks-debain-vultr.sh
+++ install-shadowsocks-debain-vultr.sh
@@ -25,7 +25,7 @@ systemctl start docker
 systemctl enable docker
 
 # Get docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # Get config file
